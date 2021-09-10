74ca0021066facc3392461fdcb55febe409b964d
Tiago Silva de Oliveira <tiagobsb84@gmail.com>
2021-09-10 01:10:10 -0300

    Criando componente

--- ByteBank-Angular/src/app/app.module.ts
+++ ByteBank-Angular/src/app/app.module.ts
@@ -1,11 +1,15 @@
+import { NovaTransferenciaComponent } from './nova transferencia/nova-transferencia.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NovaTransferenciaComponent
+  
   ],
   imports: [
     BrowserModule
